ab26cc4dedec41eadd970d586466dff5d08871d1
Geoffrey Liu <main@g-liu.com>
2016-05-28 23:05:29 -0500

    Fix broken query selector

--- js/fastr-child.js
+++ js/fastr-child.js
@@ -1,7 +1,7 @@
 // see http://codex.wordpress.org/Function_Reference/wp_enqueue_script#jQuery_noConflict_Wrappers
 jQuery( document ).ready( function( $ ) {
 
-	$( '#top-strip a[href=#]' ).click( function( evt ) {
+	$( '#top-strip a[href="#"]' ).click( function( evt ) {
 		if ( ! $( this ).hasClass( 'back-to-top' ) ) {
 			evt.preventDefault();
 		}
